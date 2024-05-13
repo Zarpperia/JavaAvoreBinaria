f6c7a768a7eb8c43935592e38272d473a212349c
Zarpperia <castiellgames@gmail.com>
2024-05-13 20:18:34 -0300

    first commit

---
+++ src/No.java
@@ -0,0 +1,37 @@
+public class No {
+    private int valor;
+    private No  esquerda;
+    private No  direita;
+    
+    public No(int valor) {
+        this.valor = valor;
+
+    }
+    public int getValor() {
+        return valor;
+    }
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+    public No getEsquerda() {
+        return esquerda;
+    }
+    public void setEsquerda(No esquerda) {
+        this.esquerda = esquerda;
+    }
+    public No getDireita() {
+        return direita;
+    }
+    public void setDireita(No direita) {
+        this.direita = direita;
+    }
+    
+    
+    public No(int valor, No esquerda, No direita) {
+        this.valor    = valor;
+        this.esquerda = esquerda;
+        this.direita  = direita;
+    }
+
+    
+}
